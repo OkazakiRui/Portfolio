e4740ee158849792ecb09cd74cefc7c172bd92dd
RuiOkazaki <okazaki200226@gmail.com>
2021-03-29 16:36:17 +0900

    update

---
+++ src/components/firstview.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="firstview">
+    <h1>firstview</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.firstview {
+  height: 100vh;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header-nav />
+    <firstview />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import firstview from "@/components/firstview.vue";
+import headerNav from "@components/headerNav.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    firstview,
+    headerNav,
   },
 };
 </script>
