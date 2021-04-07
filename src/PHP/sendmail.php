26385737f781c272cab8dc9afc0a8c0d55833861
RuiOkazaki <okazaki200226@gmail.com>
2021-04-07 18:55:35 +0900

    php修正

--- src/PHP/sendmail.php
+++ src/PHP/sendmail.php
@@ -5,15 +5,18 @@ $name = htmlspecialchars($_POST["name"],ENT_QUOTES);
 $email = htmlspecialchars($_POST["email"],ENT_QUOTES);
 $comments = htmlspecialchars($_POST["comments"],ENT_QUOTES);
 
-function convertNewline($string, $to="\n") {
-  return preg_replace("/\r\n|\r|\n/", $to, $string);
+if($name !== "" && $email !== "" && $comments !== ""){
+  function convertNewline($string, $to="\n") {
+    return preg_replace("/\r\n|\r|\n/", $to, $string);
+  }
+  
+  $comments = str_replace(array("\r\n", "\r", "\n"), '[改行]', $comments);
+  
+  file_get_contents(
+    $URL."?name=".urlencode($name)."&mail=".$email."&text=".urlencode($comments)
+  );
 }
 
-$comments = str_replace(array("\r\n", "\r", "\n"), '[改行]', $comments);
-
-file_get_contents(
-  $URL."?name=".urlencode($name)."&mail=".$email."&text=".urlencode($comments)
-);
 
 header("location: http://localhost:8080/")
 
